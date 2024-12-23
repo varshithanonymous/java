58e8cb5aad22cb03786016879e7a619c892531ca
varshithanonymous <136003240+varshithanonymous@users.noreply.github.com>
2024-12-23 19:50:31 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name=sc.nextLine();
+        System.out.println("Hello..."+name+"!");
+        sc.close();
+
+    }
+}
